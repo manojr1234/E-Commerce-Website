baefd2f8619c4c184af2396fcf312e92b4152a0e
manojr1234 <manojrajendra19@gmail.com>
2023-10-06 19:57:31 +0530

    file added

--- fabric.js-6.0.0-beta5/src/filters/index.ts
+++
@@ -1,5 +0,0 @@
-export * as filters from './filters';
-
-export { getFilterBackend, initFilterBackend } from './FilterBackend';
-export { Canvas2dFilterBackend } from './Canvas2dFilterBackend';
-export { WebGLFilterBackend } from './WebGLFilterBackend';
--- fabric.js-6.0.0-beta5/src/util/animation/AnimationFrameProvider.ts
+++
@@ -1,9 +0,0 @@
-import { getWindow } from '../../env';
-
-export function requestAnimFrame(callback: FrameRequestCallback): number {
-  return getWindow().requestAnimationFrame(callback);
-}
-
-export function cancelAnimFrame(handle: number): void {
-  return getWindow().cancelAnimationFrame(handle);
-}
